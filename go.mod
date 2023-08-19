e753735d8cb4e8b35aa10f42dd7c4e8f23c9d8a2
Onsel Akin <onsel.akin@gmail.com>
2023-08-19 14:59:05 +0300

    Improvements and refactorings

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
 	github.com/oklog/run v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
--- plugin/server.go
+++ plugin/server.go
@@ -3,11 +3,13 @@ package plugin
 import (
 	"context"
 	"fmt"
-	"log"
 	"net"
+	"os"
 
 	"github.com/compliance-framework/assessment-runtime/plugin/proto"
 	"google.golang.org/grpc"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type pluginServer struct {
@@ -27,14 +29,31 @@ func (s *pluginServer) Execute(ctx context.Context, in *proto.ActionInput) (*pro
 }
 
 func Activate(executeFunc func(in *proto.ActionInput) (*proto.ActionOutput, error)) {
+	exitCode := -1
+	defer func() {
+		if exitCode != 0 {
+			os.Exit(exitCode)
+		}
+	}()
+
+	log.SetFormatter(&log.JSONFormatter{})
+
 	listener, err := net.Listen("tcp", ":9000")
 	if err != nil {
-		panic(err)
+		log.Fatalln("Failed to listen")
+		return
 	}
+
+	defer func() {
+		listener.Close()
+	}()
 
 	s := grpc.NewServer()
 	proto.RegisterActionServiceServer(s, NewPluginServer(executeFunc))
 	if err := s.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
+	log.Tracef("Listening on %s\n", listener.Addr().String())
+	fmt.Printf("Listening on %s\n", listener.Addr().String())
 }
