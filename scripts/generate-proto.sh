15ed9010a48b71abd8ae9d1d8d5a3b8b65673074
Onsel Akin <onsela@outlook.com>
2023-08-21 19:43:20 +0300

    Added/updated files to run everything under Docker

--- scripts/generate-proto.sh
+++ scripts/generate-proto.sh
@@ -1,1 +1,3 @@
+# Generates the proto files for the action plugin - needs to be run from the root of the repo
+
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative plugins/action.proto
