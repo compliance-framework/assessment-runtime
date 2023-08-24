7c331987ca3f16c604b7de267a643dc6bb6eb076
Onsel Akin <onsela@outlook.com>
2023-08-24 16:45:08 +0300

    Partial implementation

--- go.mod
+++ go.mod
@@ -12,7 +12,15 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.7.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/hashicorp/go-hclog v0.14.1 // indirect
+	github.com/hashicorp/go-plugin v1.4.10 // indirect
+	github.com/hashicorp/yamux v0.0.0-20180604194846-3520598351bb // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/mitchellh/go-testing-interface v0.0.0-20171004221916-a61a99592b77 // indirect
+	github.com/oklog/run v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/robfig/cron/v3 v3.0.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
--- plugins/plugin.go
+++ plugins/plugin.go
@@ -1,11 +1,22 @@
 package plugins
 
-import (
-	"context"
-)
+import goplugin "github.com/hashicorp/go-plugin"
 
 type Plugin interface {
-	Init() error
-	Execute(ctx context.Context, in *ActionInput) (*ActionOutput, error)
-	Shutdown(ctx context.Context) error
+	Execute(*ActionInput) (*ActionOutput, error)
+	Shutdown() error
+}
+
+type GRPCPlugin struct {
+	goplugin.Plugin
+	Impl Plugin
+}
+
+func (p *GRPCPlugin) GRPCServer(broker *goplugin.GRPCBroker, s *goplugin.GRPCServer) error {
+	RegisterActionServiceServer(s, &ActionService{Plugin: p.Impl})
+	return nil
+}
+
+func (p *GRPCPlugin) GRPCClient(ctx *goplugin.GRPCBroker, c *goplugin.GRPCClient) (interface{}, error) {
+	return &GRPCClient{client: NewPluginClient(c)}, nil
 }
