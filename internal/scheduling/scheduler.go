9607f7130ce8cace3c0a2f86a0bef79a25cdf53c
Onsel Akin <onsela@outlook.com>
2023-09-02 11:09:36 +0300

    Moved scheduler into its own package

--- internal/scheduling/scheduler.go
+++ internal/scheduling/scheduler.go
@@ -1,7 +1,8 @@
-package plugin
+package scheduling
 
 import (
 	"context"
+	"github.com/compliance-framework/assessment-runtime/internal/assessment"
 	"github.com/compliance-framework/assessment-runtime/internal/config"
 	"github.com/robfig/cron/v3"
 	log "github.com/sirupsen/logrus"
@@ -27,7 +28,7 @@ func NewScheduler(assessmentConfigs []config.AssessmentConfig) *Scheduler {
 func (s *Scheduler) Start(ctx context.Context) {
 	for _, assessmentConfig := range s.configs {
 		_, err := s.c.AddFunc(assessmentConfig.Schedule, func() {
-			assessment, err := NewAssessmentRunner(assessmentConfig)
+			runner, err := assessment.NewRunner(assessmentConfig)
 			if err != nil {
 				log.WithFields(log.Fields{
 					"assessment-id": assessmentConfig.AssessmentId,
@@ -38,9 +39,9 @@ func (s *Scheduler) Start(ctx context.Context) {
 				// TODO: We should report this back to the control plane.
 				return
 			}
-			s.runningAssessments.Store(assessmentConfig.AssessmentId, assessment)
-			assessment.Run(ctx)
-			assessment.Stop()
+			s.runningAssessments.Store(assessmentConfig.AssessmentId, runner)
+			runner.Run(ctx)
+			runner.Stop()
 			s.runningAssessments.Delete(assessmentConfig.AssessmentId)
 		})
 		if err != nil {
@@ -61,8 +62,8 @@ func (s *Scheduler) Stop() {
 	s.runningAssessments.Range(func(key, value interface{}) bool {
 		wg.Add(1)
 		go func() {
-			assessment := value.(*AssessmentRunner)
-			assessment.Stop()
+			runner := value.(*assessment.Runner)
+			runner.Stop()
 			wg.Done()
 		}()
 		return true
