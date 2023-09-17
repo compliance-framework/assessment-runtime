7197d346a0b82b0a773bfd9ba3a84519d6133076
Onsel Akin <onsela@outlook.com>
2023-09-17 12:51:40 +0300

    Merged executor and pack into runner for the upcoming changes

--- internal/job/runner.go
+++ internal/job/runner.go
@@ -5,28 +5,117 @@ import (
 	"fmt"
 	"github.com/compliance-framework/assessment-runtime/internal/model"
 	"github.com/compliance-framework/assessment-runtime/internal/provider"
+	goplugin "github.com/hashicorp/go-plugin"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"sync"
 )
 
 type Runner struct {
-	spec     model.JobSpec
-	pack     *provider.Pack
-	executor *provider.Executor
+	spec    model.JobSpec
+	clients map[string]*goplugin.Client
 }
 
 func NewRunner(spec model.JobSpec) (*Runner, error) {
 	a := &Runner{
-		spec: spec,
+		spec:    spec,
+		clients: make(map[string]*goplugin.Client),
 	}
 
-	pack, err := provider.NewPluginPack(spec)
+	err := a.loadProviders()
 	if err != nil {
 		return nil, err
 	}
-	a.pack = pack
-	a.executor = provider.NewExecutor(pack)
+
 	return a, nil
+}
+
+func (r *Runner) loadProviders() error {
+	pluginMap := make(map[string][]model.Plugin)
+	for _, activity := range r.spec.Activities {
+		pluginMap[activity.Plugin.Package] = append(pluginMap[activity.Plugin.Package], *activity.Plugin)
+	}
+
+	ex, err := os.Executable()
+	if err != nil {
+		return err
+	}
+
+	for pkg, plugins := range pluginMap {
+		log.WithField("package", pkg).Info("Loading package")
+
+		pluginMap := make(map[string]goplugin.Plugin)
+		for _, pluginConfig := range plugins {
+			log.WithField("plugin", pluginConfig.Name).Info("Loading plugin")
+			pluginMap[pluginConfig.Name] = &provider.GrpcPlugin{}
+		}
+		pluginsPath := filepath.Join(filepath.Dir(ex), "./plugins")
+		packagePath := fmt.Sprintf("%s/%s/%s/%s", pluginsPath, pkg, plugins[0].Version, pkg)
+
+		log.WithFields(log.Fields{
+			"package":     pkg,
+			"pluginsPath": pluginsPath,
+			"packagePath": packagePath,
+		}).Info("Loading plugin package")
+
+		client := goplugin.NewClient(&goplugin.ClientConfig{
+			HandshakeConfig:  provider.HandshakeConfig,
+			Plugins:          pluginMap,
+			Cmd:              exec.Command(packagePath),
+			AllowedProtocols: []goplugin.Protocol{goplugin.ProtocolGRPC},
+		})
+
+		for _, pluginConfig := range plugins {
+			r.clients[pluginConfig.Name] = client
+		}
+	}
+
+	return nil
+}
+
+func (r *Runner) execute(name string, input *provider.ActionInput) (*provider.ActionOutput, error) {
+	client, ok := r.clients[name]
+	if !ok {
+		err := fmt.Errorf("plugin %s not found", name)
+		log.WithField("plugin", name).Error(err)
+		return nil, err
+	}
+
+	grpcClient, err := client.Client()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"plugin": name,
+			"error":  err,
+		}).Error("Failed to get GRPC client for plugin")
+		return nil, err
+	}
+
+	raw, err := grpcClient.Dispense(name)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"plugin": name,
+			"error":  err,
+		}).Error("Failed to dispense plugin")
+		return nil, err
+	}
+
+	plg := raw.(provider.Provider)
+	output, err := plg.Execute(input)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"plugin": name,
+			"error":  err,
+		}).Error("Failed to execute plugin")
+		return nil, err
+	}
+	log.WithFields(log.Fields{
+		"plugin": name,
+		"output": output,
+	}).Info("Provider executed successfully")
+
+	return output, nil
 }
 
 func (r *Runner) Run(ctx context.Context) map[string]*provider.ActionOutput {
@@ -57,7 +146,7 @@ func (r *Runner) Run(ctx context.Context) map[string]*provider.ActionOutput {
 					SSPId:        r.spec.SspId,
 				}
 
-				output, err := r.executor.Execute(pluginName, &input)
+				output, err := r.execute(pluginName, &input)
 				mu.Lock()
 				if err != nil {
 					outputs[pluginName] = &provider.ActionOutput{
@@ -78,5 +167,17 @@ func (r *Runner) Run(ctx context.Context) map[string]*provider.ActionOutput {
 }
 
 func (r *Runner) Stop() {
-	r.pack.Unload()
+	log.Info("unloading providers")
+
+	var wg sync.WaitGroup
+
+	for _, client := range r.clients {
+		wg.Add(1)
+		go func(c *goplugin.Client) {
+			defer wg.Done()
+			c.Kill()
+		}(client)
+	}
+
+	wg.Wait()
 }
