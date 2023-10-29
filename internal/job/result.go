e67994a52e99251702099a55f32cd9bcd6358c61
Onsel Akin <onsela@outlook.com>
2023-10-29 22:28:08 +0300

    Fixed the error happening while propagating the assessment results back to the control plane.

--- internal/job/result.go
+++ internal/job/result.go
@@ -1,8 +1,5 @@
 package job
 
-import "github.com/compliance-framework/assessment-runtime/internal/provider"
-
 type Result struct {
-	AssessmentId string
-	Outputs      map[string]*provider.ExecuteResult
+	Data []RunnerResult
 }
